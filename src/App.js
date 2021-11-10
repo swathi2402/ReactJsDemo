4fd497fd132baf10635d353f6d9960e247cc0a87
Swathi <swathi.h@perfios.com>
2021-11-10 10:47:17 +0530

    ADDED: Display logo using property binding technique

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/BL_logo_square_jpg.jpg'
 
 class App extends React.Component {
 
@@ -14,6 +15,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo} alt="The BrigdeLabz logo" />
       </div>
     );
   }
