960424252692398e1a4d0857d2ff9260c3ab2f38
Swathi <swathi.h@perfios.com>
2021-11-10 21:33:50 +0530

    REFACTORED: Changed indentation of js file

--- src/App.js
+++ src/App.js
@@ -33,32 +33,32 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <div className="ht-ml">
-      <div className="body-css">
-        <div>
-          <h1 className="header1">Hello {this.state.userName}, from BrigdeLabz</h1>
-          <img className="image" src={logo} onClick={this.onClick} alt="The BrigdeLabz logo" />
-        </div>
-        <div>
-          <input className="in-put" onChange={this.onNameChange} placeholder="Enter Name" />
-          <span className="error-output">{this.state.nameError}</span>
-        </div>
-        <p className="para">At BridgeLabz, we're a techie community</p>
-        <ul>
-          <li className="list">technologists</li>
-          <li className="list">thinkers</li>
-          <li className="list">builders</li>
-        </ul>
-        <p className="para">
-          BridgeLabz Solutions has introduced an Income Sharing Agreement (ISA) model which provides an opportunity for the students to pursue
-          training under the BridgeLabz CodinClub and pay when they score a job. The revolutionary model prevents financial constraints from
-          hampering the skill development of learners, structuring the fee payment after gaining employment.
-        </p>
-        <p className="para">
-          To know about us, visit <a href="https://www.bridgelabz.com/">BridgeLabz</a>
-          to learn even more about our mission i.e., <strong>Employability to all</strong>.
-        </p>
-        </div>
+        <div className="ht-ml">
+          <div className="body-css">
+            <div>
+              <h1 className="header1">Hello {this.state.userName}, from BrigdeLabz</h1>
+              <img className="image" src={logo} onClick={this.onClick} alt="The BrigdeLabz logo" />
+            </div>
+            <div>
+              <input className="in-put" onChange={this.onNameChange} placeholder="Enter Name" />
+              <span className="error-output">{this.state.nameError}</span>
+            </div>
+            <p className="para">At BridgeLabz, we're a techie community</p>
+            <ul>
+              <li className="list">technologists</li>
+              <li className="list">thinkers</li>
+              <li className="list">builders</li>
+            </ul>
+            <p className="para">
+              BridgeLabz Solutions has introduced an Income Sharing Agreement (ISA) model which provides an opportunity for the students to pursue
+              training under the BridgeLabz CodinClub and pay when they score a job. The revolutionary model prevents financial constraints from
+              hampering the skill development of learners, structuring the fee payment after gaining employment.
+            </p>
+            <p className="para">
+              To know about us, visit <a href="https://www.bridgelabz.com/">BridgeLabz</a>
+              to learn even more about our mission i.e., <strong>Employability to all</strong>.
+            </p>
+          </div>
         </div>
       </>
     );
--- src/home.js
+++ src/home.js
@@ -5,29 +5,29 @@ export default function Home() {
     return (
         <>
             <header><h2>HEADER - Sample FlexBox Example</h2></header>
-                <div class="mainContainer">
-                    <section class="mainContentItem">
-                        <article>
-                            <h2>First article</h2>
-                            <p>The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. 
-                                It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.</p>
-                        </article>
-                        <article>
-                            <h2>Second article</h2>
-                            <p>Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. 
-                                HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.</p>
-                        </article>
-                        <article>
-                            <h2>Third article</h2>
-                            <p>HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. 
-                                HTML provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items.</p>
-                            <p>HTML can embed programs written in a scripting language such as JavaScript, which affects the behavior and content of web pages. 
-                                Inclusion of CSS defines the look and layout of content.</p>
-                        </article>
-                    </section>
-                    <nav class="sideBarItem">Nav</nav>
-                    <aside class="sideBarItem">Aside</aside>
-                </div>
+            <div class="mainContainer">
+                <section class="mainContentItem">
+                    <article>
+                        <h2>First article</h2>
+                        <p>The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.
+                            It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.</p>
+                    </article>
+                    <article>
+                        <h2>Second article</h2>
+                        <p>Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages.
+                            HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.</p>
+                    </article>
+                    <article>
+                        <h2>Third article</h2>
+                        <p>HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page.
+                            HTML provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items.</p>
+                        <p>HTML can embed programs written in a scripting language such as JavaScript, which affects the behavior and content of web pages.
+                            Inclusion of CSS defines the look and layout of content.</p>
+                    </article>
+                </section>
+                <nav class="sideBarItem">Nav</nav>
+                <aside class="sideBarItem">Aside</aside>
+            </div>
             <footer><h3>Footer</h3></footer>
         </>
     );
