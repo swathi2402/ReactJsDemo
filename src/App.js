b305d80ddaaa1fe83740e7c0d59f95005a6d1a64
Swathi <swathi.h@perfios.com>
2021-11-10 10:36:47 +0530

    MODIFIED: Displaying welcome message using interpolation data binding

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      title: 'Hello from BridgeLabz'
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello from BridgeLabz</h1>
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
